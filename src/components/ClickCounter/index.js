e5c2d49131d9d5b4feefd08d6016e26bc5accce5
vanapallipremkumar <vanapallipremkumar@gmail.com>
2021-08-19 05:56:28 +0000

    Created Webpage for increasing counter on button click

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,35 @@
+// Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class ClickCounter extends Component {
+  state = {count: 0}
+
+  increase = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">The Button has been clicked</h1>
+        <h1 className="heading">
+          <span className="count">{count}</span> times
+        </h1>
+        <p>Click the button to increase the count!</p>
+        <button
+          type="button"
+          className="click-me-button"
+          onClick={this.increase}
+        >
+          Click Me!
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
